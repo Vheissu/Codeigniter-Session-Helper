7fd1e5228f926141d91696eb50422e2848e2f65e
Dwayne Charrington <dwaynecharrington@gmail.com>
2014-05-18 20:50:18 +1000

    Changed copyright year, removed ampersand assign by reference as it's not need for later versions of PHP.

--- application/helpers/session_helper.php
+++ application/helpers/session_helper.php
@@ -7,7 +7,7 @@
  *
  * @package     Codeigniter Session Helper
  * @author      Dwayne Charrington
- * @copyright   Copyright (c) 2012, Dwayne Charrington
+ * @copyright   Copyright (c) 2014, Dwayne Charrington
  * @license     http://www.apache.org/licenses/LICENSE-2.0.html
  * @link        http://ilikekillnerds.com
  * @since       Version 1.0
@@ -18,7 +18,7 @@ if (!function_exists('get_flashdata'))
 {
     function get_flashdata($key)
     {
-        $CI =& get_instance();
+        $CI = get_instance();
         $CI->load->library('session');
 
         return $CI->session->flashdata($key);
@@ -29,7 +29,7 @@ if (!function_exists('set_flashdata'))
 {
     function set_flashdata($key, $value)
     {
-        $CI =& get_instance();
+        $CI = get_instance();
         $CI->load->library('session');
 
         return $CI->session->set_flashdata($key, $value);
@@ -40,7 +40,7 @@ if (!function_exists('keep_flashdata'))
 {
     function keep_flashdata($key)
     {
-        $CI =& get_instance();
+        $CI = get_instance();
         $CI->load->library('session');
 
         return $CI->session->keep_flashdata($key);
@@ -51,7 +51,7 @@ if (!function_exists('get_userdata'))
 {
     function get_userdata($key)
     {
-        $CI =& get_instance();
+        $CI = get_instance();
         $CI->load->library('session');
 
         return $CI->session->userdata($key);
@@ -62,7 +62,7 @@ if (!function_exists('all_userdata'))
 {
     function all_userdata()
     {
-        $CI =& get_instance();
+        $CI = get_instance();
         $CI->load->library('session');
 
         return $CI->session->all_userdata();
@@ -73,7 +73,7 @@ if (!function_exists('set_userdata'))
 {
     function set_userdata($key, $value)
     {
-        $CI =& get_instance();
+        $CI = get_instance();
         $CI->load->library('session');
 
         return $CI->session->set_userdata($key, $value);
@@ -84,7 +84,7 @@ if (!function_exists('unset_userdata'))
 {
     function unset_userdata($data)
     {
-        $CI =& get_instance();
+        $CI = get_instance();
         $CI->load->library('session');
 
         return $CI->session->unset_userdata($data);
@@ -95,7 +95,7 @@ if (!function_exists('session_destroy'))
 {
     function session_destroy()
     {
-        $CI =& get_instance();
+        $CI = get_instance();
         $CI->load->library('session');
 
         return $CI->session->sess_destroy();
